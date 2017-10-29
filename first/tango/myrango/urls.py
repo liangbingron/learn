5b2bffa777816fbd90ee7690a943cd614f4fcdee
liangbing-ron <liangbing-ron@qq.com>
2017-10-29 23:03:52 +0800

    template and static

--- first/tango/myrango/urls.py
+++ first/tango/myrango/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns,url
 from myrango import views
 
 urlpatterns=patterns('',
-	url(r'about$',views.about,name='about'),
+	url(r'about/$',views.about,name='about'),
 	url(r'^$',views.index,name='index'),
 
 
--- first/tango/myrango/views.py
+++ first/tango/myrango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("Rango says: Hello world! <br/> <a href=/myrango/about>About</a>")
-
+	context_dict={'boldmessage':"I am bold font from the context"}
+	return render(request,'myrango/index.html',context_dict)
 def about(request):
 	response=HttpResponse()
 	response.write("<p>Here's the text of the about page<p>")
 	response.write("<a href=/myrango/>Index</a>")
 	return response
+
+
 # Create your views here.
